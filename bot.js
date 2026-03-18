b7fab0a49d44505637b3b255586007485d090f83
ConVerse20 <119104591+ConVerse20@users.noreply.github.com>
2026-03-18 16:18:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.TOKEN;
 
-// 🔥 PAKAI POLLING (BUKAN WEBHOOK)
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 console.log('🚀 BOT POLLING AKTIF');
 
-// TEST
 bot.on('message', (msg) => {
   console.log('📨 MASUK:', msg.text);
   bot.sendMessage(msg.chat.id, '✅ BOT HIDUP (POLLING)');
